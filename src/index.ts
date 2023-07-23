b57d1aa221b53e18f88de437476d9fc87fe68cf0
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-07-23 16:29:39 +0800

    chore: separate process function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { SQSHandler, SQSEvent } from "aws-lambda";
 import { BigCommerceCallbackPayload } from "./bigcommerce";
+import { process } from "./process";
 
 export const handler: SQSHandler = (event: SQSEvent) => {
   event.Records.forEach((record) => {
-    const callbackPayload: BigCommerceCallbackPayload = JSON.parse(record.body);
-
-    // Create the processing logic for a BigCommerce callback payload here.
-    console.log(JSON.stringify(callbackPayload));
+    const payload: BigCommerceCallbackPayload = JSON.parse(record.body);
+    process(payload);
   });
 
   return;
