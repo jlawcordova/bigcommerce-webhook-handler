68eae2af4a386f8ebba8ac007850974d6393c583
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-07-23 14:51:59 +0800

    chore: modularize gateway, sqs and lambda

--- infra/main.tf
+++ infra/main.tf
@@ -15,6 +15,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+
+resource "random_pet" "project" {
+  length = 2
+}
+
 module "bigcommerce-webhooks" {
-  source = "../modules/bigcommerce-webhooks"
+  source = "./modules/bigcommerce-webhooks"
+
+  # Use a random name when the project variable is not set.
+  project = coalesce(var.project, "bigcommerce-webhook-${random_pet.project.id}")
+  environment = var.environment
 }
---
+++ infra/modules/bigcommerce-webhooks/variable.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  description = "The name of the project. This is used for resource naming and tagging."
+  type        = string
+  default     = "bigcommerce-webhook"
+}
+
+variable "lambda_src_path" {
+  type        = string
+  default     = "/../../../src"
+  description = "The relative path of where the source code for the lambda function resides."
+}
+
+variable "environment" {
+  type        = string
+  default     = "development"
+  description = "The type of environment where the webhook handler is deployed. This is used for resource tagging."
+}
