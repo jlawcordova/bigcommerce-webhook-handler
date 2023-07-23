8f4ec817f19019b48a151f42b142fa90d717ef4c
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-07-23 15:18:17 +0800

    chore: provide lambda execution role

--- infra/modules/bigcommerce-webhooks/main.tf
+++ infra/modules/bigcommerce-webhooks/main.tf
@@ -91,7 +91,7 @@ resource "aws_sqs_queue_policy" "bigcommerce_webhook" {
   policy = templatefile("${path.module}/templates/bigcommerce-webhook-sqs-queue-policy.json",
     {
       bigcommerce_webhook_api_gateway_integration_role = aws_iam_role.bigcommerce_webhook_api_gateway_integration.arn
-      bigcommerce_webhook_lambda_role = aws_iam_role.bigcommerce_webhook_lambda.arn
+      bigcommerce_webhook_lambda_role = aws_iam_role.bigcommerce_webhook_api_gateway_integration.arn
     }
   )
 }
@@ -149,6 +149,11 @@ resource "aws_iam_role" "bigcommerce_webhook_lambda" {
     Project     = var.project
     Environment = var.environment
   }
+}
+
+resource "aws_iam_role_policy_attachment" "bigcommerce_webhook_lambda_basic_execution" {
+  role       = aws_iam_role.bigcommerce_webhook_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_event_source_mapping" "bigcommerce_webhook_sqs" {
